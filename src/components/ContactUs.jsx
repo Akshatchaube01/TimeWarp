d888a54a74e8ae2d6515b62d8053b92231d0cba0
aritra-sarkar-42 <aritrasarkar2004nov@gmail.com>
2024-05-13 00:26:25 +0530

    created the contact Us page with 3D animations and full functionality

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,19 +1,157 @@
-// components/ContactUs.jsx
-import React from 'react';
+// // components/ContactUs.jsx
+// import React from 'react';
 
-const ContactUs = () => {
+// const ContactUs = () => {
+//   return (
+//     <div className='m-8'>
+//       <h2 className='text-5xl font-bold'>ContactUs Us</h2>
+//       <div className='my-16 h-full'>
+//         <p className='text-2xl font-light'>If you have any questions or feedback, feel free to reach out to us:</p>
+//         <ul className='text-2xl my-12'>
+//           <li className=''>Email: info@timewarp.com</li>
+//           <li className=''>Phone: +1 (555) 123-4567</li>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ContactUs;
+
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+import { styles } from "../style";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "./hoc";
+import { slideIn } from "../utils/motions";
+
+const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        'service_pmgfekv',
+        'template_envuc98',
+        {
+          from_name: form.name,
+          to_name: "TimeWarp",
+          from_email: form.email,
+          to_email: "info@timewarp.com",
+          message: form.message,
+        },
+        '/* in emailjs website the owner would require to generate their particular idenetity code with the assigned mail address */'
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert("Something went Wrong!");
+        }
+      );
+  };
+
   return (
-    <div className='m-8'>
-      <h2 className='text-5xl font-bold'>Contact Us</h2>
-      <div className='my-16 h-full'>
-        <p className='text-2xl font-light'>If you have any questions or feedback, feel free to reach out to us:</p>
-        <ul className='text-2xl my-12'>
-          <li className=''>Email: info@timewarp.com</li>
-          <li className=''>Phone: +1 (555) 123-4567</li>
-        </ul>
-      </div>
+    <div
+      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+    >
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact Us.</h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className='mt-12 flex flex-col gap-8'
+        >
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Name</span>
+            <input
+              type='text'
+              name='name'
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your good name?"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your email</span>
+            <input
+              type='email'
+              name='email'
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your web address?"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Message</span>
+            <textarea
+              rows={7}
+              name='message'
+              value={form.message}
+              onChange={handleChange}
+              placeholder='What you want to say?'
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+
+          <label className="flex flex-col">
+          <button
+            type='submit'
+            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary bg-cyan-800'
+          >
+            {loading ? "Sending..." : "Send"}
+          </button>
+          </label>
+        </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+      >
+        <EarthCanvas />
+      </motion.div>
     </div>
   );
-}
+};
 
-export default ContactUs;
+export default SectionWrapper(Contact, "contact");
