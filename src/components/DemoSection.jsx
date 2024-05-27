6d30c67b52c0cd00ecad9ee267115c9952315c67
SrinivasDevolper <kingsrinivassri123@gmail.com>
2024-05-27 10:13:22 +0530

    Resolved conflicts and updated feature branch

--- src/components/DemoSection.jsx
+++ src/components/DemoSection.jsx
@@ -19,6 +19,7 @@ const DemoSection = () => {
 
   return (
     <div className="demo-container md:px-32">
+      
       <h2 className="text-5xl font-bold my-16">Demo Section</h2>
       <div className="md:w-2/3 mx-auto">
         <div
