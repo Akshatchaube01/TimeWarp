813d2acee97ec44df670cfafe5dd186d9345e9e3
Rohitha Pudu <rohithapudu@gmail.com>
2024-05-28 00:10:44 +0530

    update src/components/ComparePlaces.jsx

--- src/components/ComparePlaces.jsx
+++ src/components/ComparePlaces.jsx
@@ -3,7 +3,7 @@ import UnsplashImageGallery from './Unsplash';
 
 const ComparePlaces = () => {
   // Replace 'ACCESS_KEY' with your actual Unsplash API Access Key
-  const accessKey = 'ACCESS_KEY';
+  const accessKey = '1wKSrYCMbn8UDTUbrhqhlxnhIluHoE573xItr-V2lMA';
   const count = 1;
 
   return (
