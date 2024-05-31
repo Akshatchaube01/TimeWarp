0779ac14b799e3e28d84c2124ef65a449698bcba
say-het <iam.lover.of.nature99@gmail.com>
2024-05-31 14:16:29 +0530

    Revert "Fixed Footer Design"
    This reverts commit 8a9fa2c6d76fd7001ef422bf20f4cdb62df4120d.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,90 +1,53 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-  return (
-    <footer className="footer">
-      <div className="footer-content">
-        <div className="footer-section contact-details">
-          <h4><strong>Contact Us</strong></h4>
-          <ul className="contact-list">
-            <li>
-              <i className="fas fa-envelope"></i>&nbsp;&nbsp;
-              <a href="mailto:akshatchaube22@gmail.com">akshatchaube22@gmail.com</a>
-            </li>
-            <li>
-              <i className="fas fa-phone"></i>&nbsp;&nbsp;
-              <a href="tel:+91 1234987619">+91 1234987619</a>
-            </li>
-          </ul>
-        </div>
-        <div className="footer-section navigation">
-          <h4><strong>Navigation</strong></h4>
-          <ul className="nav-list">
-            <li>
-              <a href="/" className="nav-link">
-                <i className="fas fa-home"></i>&nbsp;&nbsp;Home
-              </a>
-            </li>
-            <li>
-              <a href="/AboutUs" className="nav-link">
-                <i className="fas fa-user"></i>&nbsp;&nbsp;About Us
-              </a>
-            </li>
-            <li>
-              <a href="/DemoSection" className="nav-link">
-                <i className="fas fa-cogs"></i>&nbsp;&nbsp;Services
-              </a>
-            </li>
-            <li>
-              <a href="/ContactUs" className="nav-link">
-                <i className="fas fa-envelope"></i>&nbsp;&nbsp;Contact
-              </a>
-            </li>
-            <li>
-              <a href="/Models" className="nav-link">
-                <i className="fas fa-database"></i>&nbsp;&nbsp;Models
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="footer-section quote">
-          <h4><strong>Quote</strong></h4>
-          <p>
-            <cite>Innovation distinguishes between a leader and a follower.</cite> - Steve Jobs
-          </p>
-        </div>
-        <div className="footer-section social-media">
-          <h4><strong>Follow Us</strong></h4>
-          <ul className="social-list">
-            <li>
-              <a href="https://github.com/Akshatchaube01/TimeWarp" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-github"></i>&nbsp;&nbsp; Github
-              </a>
-            </li>
-            <li>
-              <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-twitter"></i>&nbsp;&nbsp; Twitter
-              </a>
-            </li>
-            <li>
-              <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-instagram"></i>&nbsp;&nbsp; Instagram
-              </a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/akshat-chaube-b2b042226/" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-linkedin-in"></i>&nbsp;&nbsp; LinkedIn
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="footer-bottom">
-        <p>&copy; 2024 TimeWarp. All rights reserved.</p>
-      </div>
-    </footer>
-  );
-};
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <div className="footer-section contact-details">
+                    <h4><strong>Contact Us</strong></h4>
+                    <p>Email: <a href="mailto:akshatchaube22@gmail.com">akshatchaube22@gmail.com</a></p>
+                    <p>Phone: <a href="tel:+1234567890">+91 1234987619</a></p>
+                </div>
+                <div className="footer-section navigation">
+                    <h4><strong>Navigation</strong></h4>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/AboutUs">About Us</a></li>
+                        <li><a href="/DemoSection">Services</a></li>
+                        <li><a href="/ContactUs">Contact</a></li>
+                        <li><a href="/Models">Models</a></li>
+                    </ul>
+                </div>
+                <div className="footer-section quote">
+                    <h4><strong>Quote</strong></h4>
+                    <p><cite>Innovation distinguishes between a leader and a follower.</cite> - Steve Jobs</p>
+                </div>
+                <div className="footer-section social-media">
+                    <h4><strong>Follow Us</strong></h4>
+                    <div className='smedia'>
+                    <a href="https://github.com/Akshatchaube01/TimeWarp" target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-github"></i>
+                    </a>
+                    <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-instagram"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/akshat-chaube-b2b042226/" target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-linkedin-in"></i>
+                    </a>
+                    </div>
+                </div>
+            </div>
+            <div className="footer-bottom">
+                <p>&copy; 2024 TimeWarp. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+}
 
 export default Footer;
